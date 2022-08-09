a1bc6b8e2a2b340c973f78b1b9c205be1b365094
kjstarun <103061882+kjstarun@users.noreply.github.com>
2022-08-09 18:36:22 +0530

    Update reverse.py
    Removed inbuilt functions

--- reverse.py
+++ reverse.py
@@ -1,7 +1,10 @@
 # to reverse array
 
 def rev(l):
-    r = l[::-1]
+    r = []
+    s = []
+    for i in range(1,len(l)+1):
+        r = r + [l[-i]]
     return r
 l = [10, 56, 67, -45, -2, 55, 78, 56, -4, 100, 200, 45]
-print(rev(l))
+print("The reversed array is",rev(l))
