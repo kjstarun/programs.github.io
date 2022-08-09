6489a04bd22ad5aaab3738cf2ade3ce6e664e8f3
kjstarun <103061882+kjstarun@users.noreply.github.com>
2022-08-09 18:51:02 +0530

    Update sort.py
    Removed inbuilt functions.

--- sort.py
+++ sort.py
@@ -1,7 +1,10 @@
 # to sort in ascending order
 
-def asc(l):
-    l.sort()
+def rev(l):
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            if l[i] > l[j]:
+                l[i], l[j] = l[j], l[i]
     return l
 l = [10, 56, 67, -45, -2, 55, 78, 56, -4, 100, 200, 45]
-print(asc(l))
+print("The sorted array is",rev(l))
