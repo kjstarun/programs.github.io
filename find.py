69ae5c0ca6ddff45799ea1585ff4a1c3ea1ff1b7
kjstarun <103061882+kjstarun@users.noreply.github.com>
2022-08-09 19:02:08 +0530

    Update find.py
    Removde inbuilt function.

--- find.py
+++ find.py
@@ -1,12 +1,22 @@
 # to find 3rd element
 
 def findsmall(k,l):
-    l.sort()
-    a = l[k-1]
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            if l[i] > l[j]:
+                l[i], l[j] = l[j], l[i]
+    a = []
+    for i in range(k):
+        a = a + [l[i]]
     return a
 def findbig(k,l):
-    l.sort()
-    b = l[-k]
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            if l[i] > l[j]:
+                l[i], l[j] = l[j], l[i]
+    b = []
+    for i in range(1,k+1):
+        b = b + [l[-i]]
     return b
 l = [10, 56, 67, -45, -2, 55, 78, 56, -4, 100, 200, 45]
 k = int(input())
