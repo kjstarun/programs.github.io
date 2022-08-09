acc7c0d7cd6fbf402677591f9b8c900d8e48ec51
kjstarun <103061882+kjstarun@users.noreply.github.com>
2022-08-09 16:09:42 +0530

    Add files via upload

---
+++ minmax.py
@@ -0,0 +1,11 @@
+# to find minimum and maximum
+
+def mini(l):
+    a = min(l)
+    return a
+def maxi(l):
+    b = max(l)
+    return b
+l = [10, 56, 67, -45, -2, 55, 78, 56, -4, 100, 200, 45]
+print("Minimum element is",mini(l))
+print("Maximum element is",maxi(l))
