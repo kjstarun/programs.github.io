138a408e3c32bd81ffda6e629005700e1024c8f6
kjstarun <103061882+kjstarun@users.noreply.github.com>
2022-08-09 17:41:19 +0530

    Update minmax.py
    Removed inbuilt functions.

--- minmax.py
+++ minmax.py
@@ -1,11 +1,17 @@
 # to find minimum and maximum
 
 def mini(l):
-    a = min(l)
+    a = 0
+    for i in range(len(l)):
+        if l[i] < a:
+            a = l[i]
     return a
 def maxi(l):
-    b = max(l)
+    b = 0
+    for i in range(len(l)):
+        if l[i] > b:
+            b = l[i]
     return b
 l = [10, 56, 67, -45, -2, 55, 78, 56, -4, 100, 200, 45]
-print("Minimum element is",mini(l))
-print("Maximum element is",maxi(l))
+print("The minimum element in the array is",mini(l))
+print("The maximum element in the array is",maxi(l))
