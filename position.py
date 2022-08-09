72ec3f43ecd919e8eab073eda1310074355e67ec
kjstarun <103061882+kjstarun@users.noreply.github.com>
2022-08-09 17:35:45 +0530

    Update position.py
    Removed inbuilt functions.

--- position.py
+++ position.py
@@ -1,10 +1,13 @@
 #To find the position
 
 def pos(a):
-    for i in l:
-        if i == a:
-            r = l.index(i)
-    return r + 1
+    for i in range(len(l)):
+        if l[i] == a:
+            r = i+1
+            break
+        else:
+            r = -99999
+    return r
 l = [10, 56, 67, -45, -2, 55, 78, 56, -4, 100, 200, 45]
 a = int(input())
-print(pos(a))
+print("The Position of the element",a,"is",pos(a))
